b002f06bd35f5ff65a7f41ede1a27c132b5b317d
Tyler Palmerton <104020051+tpalmerton@users.noreply.github.com>
2024-10-11 14:06:06 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,15 +11,25 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     # Path for logout
-     path('logout/', views.logout_request, name='logout'),
+    path('logout/', views.logout_request,
+          name='logout'),
     # path for registration
-    path(route="register", view=views.registration, name="register"),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route="register", view=views.registration,
+         name="register"),
+    path(route='get_cars', view=views.get_cars,
+         name ='getcars'),
+    path(route='get_dealers/',
+         view=views.get_dealerships,
+         name='get_dealers'),
+    path(route='get_dealers/<str:state>',
+         view=views.get_dealerships,
+         name='get_dealers_by_state'),
     # path for dealer reviews view
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>',
+         view=views.get_dealer_details,
+         name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>',
+         view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
     path(route='add_review', view=views.add_review, name='add_review'),
 
