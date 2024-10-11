f060985d827b324ce5064e8d0d4e56dc8c6d1e20
Tyler Palmerton <104020051+tpalmerton@users.noreply.github.com>
2024-10-11 13:29:23 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,9 +29,12 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 
-                 'https://tdpalmerton-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://tdpalmerton-8000.
+                 theiadockernext-0-labs-prod-theiak8s-4-tor01.
+                 proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://tdpalmerton-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    'https://tdpalmerton-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.
+    proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -96,19 +99,23 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.
+        UserAttributeSimilarityValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'django.contrib.auth.password_validation.
+        MinimumLengthValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'django.contrib.auth.password_validation.
+        CommonPasswordValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'django.contrib.auth.password_validation.
+        NumericPasswordValidator',
     },
 ]
 
