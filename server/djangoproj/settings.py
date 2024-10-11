21d9fb009b2fe24d39beb1219fe409c254a6c494
Tyler Palmerton <104020051+tpalmerton@users.noreply.github.com>
2024-10-11 13:43:06 -0500

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,12 +29,11 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 
-                 'https://tdpalmerton-8000.
-                 theiadockernext-0-labs-prod-theiak8s-4-tor01.
-                 proxy.cognitiveclass.ai']
+                 'https://tdpalmerton-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+                 'proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = [
-    'https://tdpalmerton-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.
-    proxy.cognitiveclass.ai']
+    'https://tdpalmerton-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+    'proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
